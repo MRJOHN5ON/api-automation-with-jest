75031abbf97284d6146f97e261ae2f7139b8827c
Ryley Johnson <ryleyjohnsonemail@gmail.com>
2024-08-17 22:44:51 -0700

    title fix

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -2,11 +2,7 @@
 const config = require('../config');
 
 //first test: checking if database returns with http code 200
-<<<<<<< HEAD
 test('Should return HTTP status code 200', async () => {
-=======
-test('Should respond with status code 200', async () => {
->>>>>>> 4a000a2fedb0b9e1ec3616cabf0df9b890f39ecc
 	let actualResponse;
 	try {
 		const response = await fetch(`${config.API_URL}${config.GET_ENDPOINT}`);
@@ -18,11 +14,7 @@ test('Should respond with status code 200', async () => {
 });
 
 // second test to ensure response body gives database info
-<<<<<<< HEAD
 test('Should return a response body, "productsList"', async () => {
-=======
-test('Should contain productsList in response body', async () => {
->>>>>>> 4a000a2fedb0b9e1ec3616cabf0df9b890f39ecc
 	let actualBody;
 	try {
 		const response = await fetch(`${config.API_URL}${config.GET_ENDPOINT}`);
