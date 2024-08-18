96848ebebe0438155fc4b86028f6e42f6895ec05
Ryley Johnson <ryleyjohnsonemail@gmail.com>
2024-08-17 22:48:51 -0700

    resubmission

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-ac4adc26-6cbb-46d3-b8f7-ad72d20dd976.containerhub.tripleten-services.com",
+    API_URL: "https://cnt-a421bc07-40cd-4f6e-b43d-511d31fe807b.containerhub.tripleten-services.com",
     GET_ENDPOINT: "/api/db/resources/kit_model.csv",
     POST_ENDPOINT: "/fast-delivery/v3.1.1/calculate-delivery.xml",
     PUT_ENDPOINT:"/api/v1/products/5",
--- tests/postDelete.test.js
+++ tests/postDelete.test.js
@@ -1,4 +1,4 @@
-
+// eslint-disable-next-line no-undef
 const config = require('../config');
 
 
